2e5ea7624a2484d8ca2a429ed5016ebe88a515be
LauraGramolini <lauragramolini@gmail.com>
2023-08-03 17:32:56 +0200

    Camallanus scripts and data

--- Camallanus/Bash_scripts/genome/02_fastuniq.sh
+++ Camallanus/Bash_scripts/genome/02_fastuniq.sh
@@ -2,6 +2,7 @@
 
 
 fastuniq \
--i fastuniqfile.txt \
+-i /SAN/Lauras_territory/cama_genome/Miseq_2023/fastuniq/fastuniqfile.txt \
+-tq \
 -o /SAN/Lauras_territory/cama_genome/Miseq_2023/fastuniq/camafastuniq_F.fq \
 -p /SAN/Lauras_territory/cama_genome/Miseq_2023/fastuniq/camafastuniq_R.fq
